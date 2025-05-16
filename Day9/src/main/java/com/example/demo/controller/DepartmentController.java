d020e7350880b264ca0096a1c6030bbb359e2bb2
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-16 23:06:12 +0530

    Added Some details of DepartmntController and added service

--- Day9/src/main/java/com/example/demo/controller/DepartmentController.java
+++ Day9/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.DepartmentService;
+
+@RestController
+@RequestMapping("/dept")
 public class DepartmentController {
 
+	@Autowired
+	private DepartmentService service;
+	
 }
