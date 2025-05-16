42f9974a9cde598b079100092af5d2dcd73ae37d
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-16 23:57:19 +0530

    AddedDetails of the application

--- Day9/src/test/java/com/example/demo/Day9ApplicationTests.java
+++ Day9/src/test/java/com/example/demo/Day9ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Day9ApplicationTests {
+public class Day9ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	 public static void main(String[] args) {
+	        SpringApplication.run(Day9Application.class, args);
+	    }
 
 }
